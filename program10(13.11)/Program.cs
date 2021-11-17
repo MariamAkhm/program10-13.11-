942bfce99e97fe8aa7b0fb537e69589ddd4aec2d
Mariam.Akhm <mahmetsafina7@gmail.com>
2021-11-17 23:54:30 +0300

    Добавьте файлы проекта.

---
+++ program10(13.11)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program10_13._11_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
